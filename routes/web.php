fce6abf2c35b0bd39dcd943ac5a4ea05ac671384
Margarida <a78376@alunos.uminho.pt>
2020-07-02 15:09:12 +0100

    App

--- routes/web.php
+++ routes/web.php
@@ -12,41 +12,59 @@ use Illuminate\Support\Facades\Route;
   | contains the "web" middleware group. Now create something great!
   |
  */
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/list', ['uses' => 'publicacoesController@index']);
+Auth::routes();
 
-Route::resource('publications', 'publicacoesController');
+Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/list', 'publicacoesController@index');
+Route::get('/list', 'PublicationController@index');
 
-Route::get('/info', 'Controller@index');
-//Route::get('/password', function() {
-//    return view('auth.passwords.reset');
-//});
+Route::get('/info', 'InfoPessoalController@index');
+
+Route::post('/publicar', 'PublicationController@store');
+
+Route::delete('/home/{idPublication}', 'PublicationController@destroy');
+
+Route::get('/list/{idPublication}', 'PublicationController@destroy');
+
+
+
+
+
+//
+//Route::get('/list', ['uses' => 'publicacoesController@index']);
+//
+//Route::resource('publications', 'publicacoesController');
+//
+//Route::get('/list', 'publicacoesController@index');
+//
+//Route::get('/info', 'Controller@index');
+Route::get('/password', function() {
+        return view('auth.passwords.reset');
+});
 
 Route::get('/change-password', 'ChangePasswordController@index');
 Route::post('change-password', 'ChangePasswordController@store')->name('change.password');
 
-//Route::get('/auth/passwords/reset', 'publicacoesController@index');
-
-Route::delete('/list/{idPublication}', 'publicacoesController@destroy');
-
-//Route::get('/reset', 'ResetPasswordController@showChangePasswordForm');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
-
+Route::get('/auth/passwords/reset', 'publicacoesController@index');
+//
+//Route::delete('/home/{idPublication}', 'publicacoesController@destroy');
+//
+Route::get('/reset', 'ResetPasswordController@showChangePasswordForm');
+//
+//Auth::routes();
+//
+//Route::get('/home', 'HomeController@index')->name('home');
+//
+//Auth::routes();
+//
+//Route::get('/home', 'HomeController@index')->name('home');
+//
+//Auth::routes();
+//
+//Route::get('/home', 'HomeController@index')->name('home');
+//
+//
